216b17186c51d33760ba6a909b2c467d69f5e518
Antonio Thomacelli <tonnytg@gmail.com>
2024-07-22 10:30:00 -0300

    fix: add new regex to block wrong domains

--- chkmail.go
+++ chkmail.go
@@ -1,9 +1,18 @@
 package chkmail
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 // ChkReg will check regex for Email Expression content with @ and one .
 func ChkReg(n string) bool {
-	var emailRegex = regexp.MustCompile(`(?!(^[.-]|^.*?[.]{2,}|[.-]$))[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$`)
-	return emailRegex.MatchString(n)
+	var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(n) {
+		return false
+	}
+	// check for invalid domain patterns
+	var invalidDomainPatterns = regexp.MustCompile(`(^-|-$|\.{2,})`)
+	domain := n[strings.Index(n, "@")+1:]
+	return !invalidDomainPatterns.MatchString(domain)
 }
